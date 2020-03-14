4c68638c762b5358c70bf4bdfaffd6e3cdd77b8c
jcoops22 <jcooper232074@gmail.com>
2020-03-14 00:14:48 -0600

    dfad

--- logic.js
+++ logic.js
@@ -166,5 +166,5 @@ function reset() {
   computerResult.style.visibility = "hidden";
   startDiv.style.display = "flex";
   modal.style.display = "block";
-  winnerDiv.style.top = "-44vh";
+  winnerDiv.style.top = "-46vh";
 }
